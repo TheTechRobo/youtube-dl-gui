99dc5ed80cd7cc1ab2ebc17bf03346c23c931c0e
ittussaromYno3 <computer@Anfunfuns-MacBook-Air.local>
2020-06-01 10:12:37 -0400

    License

--- ytdlgui.py
+++ ytdlgui.py
@@ -6,6 +6,8 @@ from subprocess import Popen, PIPE
 
 window = Tk()
 
+mbox.showinfo("Copyright","Copyleft (c) 2020 TheTechRobo. Licensed under the GPLv3.")
+mbox.showinfo("Copyright","You should have received a copy with this Software. Else, go to www.raw.githubusercontent.com/thetechrobo/youtube-dl-gui/master/LICENSE")
 def commence():
     mbox.showinfo("...","Commencing download...")
     url = Widgets.video.get()
