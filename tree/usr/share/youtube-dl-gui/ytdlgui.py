49cec02246efd811b58f8a6d95ba5f54a7c3f572
ittussarom retals mail ynohtna <thetechrobo@penguin>
2020-11-28 11:06:06 -0500

    almost working but not yet

--- tree/usr/share/youtube-dl-gui/ytdlgui.py
+++ tree/usr/share/youtube-dl-gui/ytdlgui.py
@@ -2,7 +2,7 @@
 
 infos = {"author": "TheTechRobo", "license": "GPLv3", "type": "gui", "category": "internet", "category2": "video", "category3": "python"}
 
-from tkinter import Button, Tk, Entry, Toplevel, PhotoImage, Label
+from tkinter import Button, Tk, Entry, Toplevel, PhotoImage, Label, Frame, YES#, BOTH
 from tkinter import messagebox as mbox
 from subprocess import Popen
 from sys import stdout as sstdout
@@ -12,18 +12,19 @@ window = Tk()
 mbox.showinfo("Copyright","Copyleft (c) 2020 TheTechRobo. Licensed under the GPLv3.")
 mbox.showinfo("Copyright","You should have received a copy with this Software. Else, go to http://raw.githubusercontent.com/thetechrobo/youtube-dl-gui/master/LICENSE")
 def commence():
-    mbox.showinfo("...","Commencing download...")
+    mbox.showinfo("Commencing download...","Press OK to start...")
     url = Widgets.video.get()
     load = Toplevel(window)
     image = PhotoImage(file='/usr/share/youtube-dl-gui/pleasewait.png')
-    Label(load, image=image).pack()
+    Label(load, image=image).pack(fill="both", expand=YES, side="top")
+    termf = Frame(load, height=50, width=200)
+    termf.pack(side="bottom", fill="both", expand=YES) #https://stackoverflow.com/questions/37017472/python-tkinter-place-put-frame-to-the-bottom
+    wid = termf.winfo_id()
     try:
-        hi = Popen(["youtube-dl", url], shell=False, stdout=sstdout, stderr=sstdout)
+        Popen(['xterm -into %d -geometry 200x50 -sb -e /bin/sh -c "youtube-dl %s;exit"' % (wid, url)], shell=True)
     except Exception as ename:
         mbox.showerror("ERROR!", "An error occured.")
         print(ename)
-    else:
-        print(hi.communicate())
 
 class Widgets:
     video = Entry(window)
