f6ac57bb29cb5a856ac3a7875816789dd20a825b
ittussarom retals mail ynohtna <thetechrobo@penguin>
2020-11-29 12:21:26 -0500

    Commit early; commit often

--- tree/usr/share/youtube-dl-gui/ytdlgui.py
+++ tree/usr/share/youtube-dl-gui/ytdlgui.py
@@ -14,14 +14,16 @@ mbox.showinfo("Copyright","You should have received a copy with this Software. E
 def commence():
     mbox.showinfo("Commencing download...","Press OK to start...")
     url = Widgets.video.get()
+    if url == "":
+        mbox.showerror("Error", "Next time please type a URL.")
     load = Toplevel(window)
     image = PhotoImage(file='/usr/share/youtube-dl-gui/pleasewait.png')
-    Label(load, image=image).pack(fill="both", expand=YES, side="top")
+    Label(load, image=image).pack(fill="both", expand=False, side="top")
     termf = Frame(load, height=50, width=200)
     termf.pack(side="bottom", fill="both", expand=YES) #https://stackoverflow.com/questions/37017472/python-tkinter-place-put-frame-to-the-bottom
     wid = termf.winfo_id()
     try:
-        Popen(['xterm -into %d -geometry 200x50 -sb -e /bin/sh -c "youtube-dl %s;exit"' % (wid, url)], shell=True)
+        Popen(['xterm -into %d -geometry 200x50 -sb -e /bin/sh -c "youtube-dl %s;exit"' % (wid, url)], stdout=sstdout, stderr=sstdout, shell=True)
     except Exception as ename:
         mbox.showerror("ERROR!", "An error occured.")
         print(ename)
