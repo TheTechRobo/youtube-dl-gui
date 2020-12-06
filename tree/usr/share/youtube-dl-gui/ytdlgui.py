290efb4ef42ae1a14c567f4d53a17b5762813052
ittussarom retals mail ynohtna <thetechrobo@penguin>
2020-12-06 11:31:31 -0500

    IT WORKS !! :D

--- tree/usr/share/youtube-dl-gui/ytdlgui.py
+++ tree/usr/share/youtube-dl-gui/ytdlgui.py
@@ -9,24 +9,31 @@ from sys import stdout as sstdout
 
 window = Tk()
 
+def getPhoto(window):
+    image = PhotoImage(file='/usr/share/youtube-dl-gui/pleasewait.png')
+    Label(window, image=image).pack(fill="both", expand=False, side="top")
+    return image
+
 mbox.showinfo("Copyright","Copyleft (c) 2020 TheTechRobo. Licensed under the GPLv3.")
 mbox.showinfo("Copyright","You should have received a copy with this Software. Else, go to http://raw.githubusercontent.com/thetechrobo/youtube-dl-gui/master/LICENSE")
 def commence():
+    global photo
     mbox.showinfo("Commencing download...","Press OK to start...")
     url = Widgets.video.get()
     if url == "":
         mbox.showerror("Error", "Next time please type a URL.")
     load = Toplevel(window)
-    image = PhotoImage(file='/usr/share/youtube-dl-gui/pleasewait.png')
-    Label(load, image=image).pack(fill="both", expand=False, side="top")
-    termf = Frame(load, height=50, width=200)
+    load.geometry("1000x1000")
+    photo = getPhoto(load)
+    termf = Frame(load, height=50, width=100)
     termf.pack(side="bottom", fill="both", expand=YES) #https://stackoverflow.com/questions/37017472/python-tkinter-place-put-frame-to-the-bottom
     wid = termf.winfo_id()
     try:
-        Popen(['xterm -into %d -geometry 200x50 -sb -e /bin/sh -c "youtube-dl %s;exit"' % (wid, url)], stdout=sstdout, stderr=sstdout, shell=True)
+        Popen(['xterm -into %d -geometry 100x50 -sb -e /bin/sh -c "youtube-dl %s;exit"' % (wid, url)], stdout=sstdout, stderr=sstdout, shell=True)
     except Exception as ename:
         mbox.showerror("ERROR!", "An error occured.")
         print(ename)
+    return photo
 
 class Widgets:
     video = Entry(window)
