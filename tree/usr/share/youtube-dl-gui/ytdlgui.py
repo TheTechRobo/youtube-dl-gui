6ce19dd7cf8a5e79ac2e099fdde6d86e3ef4a82c
ittussarom retals mail ynohtna <thetechrobo@penguin>
2020-12-06 20:44:24 -0500

    Add Cancelbutton

--- tree/usr/share/youtube-dl-gui/ytdlgui.py
+++ tree/usr/share/youtube-dl-gui/ytdlgui.py
@@ -26,12 +26,18 @@ mbox.showinfo("Copyright","Copyleft (c) 2020 TheTechRobo. Licensed under the GPL
 mbox.showinfo("Copyright","You should have received a copy with this Software. Else, go to http://raw.githubusercontent.com/thetechrobo/youtube-dl-gui/master/LICENSE")
 
 def musicbox(wid, url, load):
+    global command
     if Variables.audioSelect.get():
         player = MPyg321Player()
         player.play_song("/usr/share/youtube-dl-gui/loading_music.mp3")
-    Popen(['xterm -into %d -geometry 200x50 -sb -e /bin/sh -c "youtube-dl %s;sleep 1;exit"' % (wid, url)], stdout=sstdout, stderr=sstdout, shell=True).wait()
+    command = Popen(['exec xterm -into %d -geometry 200x50 -sb -e /bin/sh -c "youtube-dl %s;sleep 1;exit"' % (wid, url)], stdout=sstdout, stderr=sstdout, shell=True)
+    command.wait()
     player.stop()
     load.destroy()
+
+def cancel():
+    mbox.showinfo("Cancelling...", "NOTE : Cancelling may leave some temporary files in the current folder; they are safe to delete.\nPlease press OK to cancel...")
+    command.terminate()
 
 def commence():
     global photo #This is necessary because https://stackoverflow.com/a/16424553/9654083
@@ -47,6 +53,7 @@ def commence():
     termf = Frame(load, height=50, width=200)
     termf.pack(side="bottom", fill="both", expand=YES) #https://stackoverflow.com/questions/37017472/python-tkinter-place-put-frame-to-the-bottom
     wid = termf.winfo_id()
+    Button(load, text="CANCEL PROCESS", command=cancel).pack()
     try:
         thread = threading.Thread(target=musicbox, daemon=True, args=(wid, url, load)) #https://realpython.com/intro-to-python-threading/
         thread.start()
